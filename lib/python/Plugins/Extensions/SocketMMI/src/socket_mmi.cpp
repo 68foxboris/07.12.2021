3f3c86a0444ee6c2068b2f6f3541d59f5b40dfd9
Boris <boris.fox.solovyev@gmail.com>
2021-12-17 19:15:33 +0200

    Update socket_mmi.cpp

--- lib/python/Plugins/Extensions/SocketMMI/src/socket_mmi.cpp
+++ lib/python/Plugins/Extensions/SocketMMI/src/socket_mmi.cpp
@@ -11,8 +11,6 @@
 #include <lib/base/estring.h>
 #include <lib/dvb_ci/dvbci_session.h>
 
-#include <Python.h>
-
 #define MAX_LENGTH_BYTES 4
 #define MIN_LENGTH_BYTES 1
 //#define MMIDEBUG
@@ -471,30 +469,30 @@ static PyMethodDef module_methods[] = {
 	},
 	{NULL, NULL, 0, NULL}   /* Sentinel */
 };
-#if PY_MAJOR_VERSION < 3
+
+#if PY_MAJOR_VERSION >= 3
+	static struct PyModuleDef moduledef = {
+		PyModuleDef_HEAD_INIT,
+		"socketmmi",											/* m_name */
+		"Module that implements mmi via unix domain socket.",	/* m_doc */
+		-1,														/* m_size */
+		module_methods,											/* m_methods */
+		NULL,													/* m_reload */
+		NULL,													/* m_traverse */
+		NULL,													/* m_clear */
+		NULL,													/* m_free */
+	};
+#endif
+
+
 PyMODINIT_FUNC
 initsocketmmi(void)
 {
+#if PY_MAJOR_VERSION >= 3
+	PyModule_Create(&moduledef);
+#else
 	Py_InitModule3("socketmmi", module_methods,
 		"Module that implements mmi via unix domain socket.");
-}
-#else
-static struct PyModuleDef socketmmi_moduledef = {
-	PyModuleDef_HEAD_INIT,
-	"socketmmi",											/* m_name */
-	"Module that implements mmi via unix domain socket.",	/* m_doc */
-	-1,														/* m_size */
-	module_methods,											/* m_methods */
-	NULL,													/* m_reload */
-	NULL,													/* m_traverse */
-	NULL,													/* m_clear */
-	NULL,													/* m_free */
-};
-
-
-PyMODINIT_FUNC PyInit_socketmmi(void)
-{
-	return PyModule_Create(&socketmmi_moduledef);
-}
 #endif
+}
 };
