f3cc38ca34c8a1531e93239a836b05f047dd6c8b
Boris <boris.fox.solovyev@gmail.com>
2022-02-06 07:45:25 +0200

    [StartWizard] Check for bytes/strings in dataAvail()
    eConsoleAppContainer() passes bytes to dataAvail(), but we also use the latter directly in appClosed() with text strings. So, we have to check what kind of data is received.
    
    thanks @athoik

--- lib/python/Screens/StartWizard.py
+++ lib/python/Screens/StartWizard.py
@@ -136,6 +136,8 @@ class AutoInstallWizard(Screen):
 			self.appClosed(True)
 
 	def dataAvail(self, data):
+		if isinstance(data, bytes):
+			data = data.decode()
 		self["AboutScrollLabel"].appendText(data)
 		self.logfile.write(data)
 
