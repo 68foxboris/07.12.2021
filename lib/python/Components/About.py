7ffe31578278b3164a62c7cb13f2ca749d8f0fbe
Boris <boris.fox.solovyev@gmail.com>
2022-02-07 10:33:07 +0200

    fix network screen about

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -269,25 +269,36 @@ def getPythonVersionString():
 
 
 def GetIPsFromNetworkInterfaces():
-	structSize = 40 if maxsize > 2 ** 32 else 32
-	sock = socket(AF_INET, SOCK_DGRAM)
-	maxPossible = 8  # Initial value.
+	import socket
+	import fcntl
+	import struct
+	import array
+	import sys
+	is_64bits = sys.maxsize > 2**32
+	struct_size = 40 if is_64bits else 32
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	max_possible = 8 # initial value
 	while True:
-		_bytes = maxPossible * structSize
-		names = array("B")
-		for index in range(_bytes):
+		_bytes = max_possible * struct_size
+		names = array.array('B')
+		for i in range(0, _bytes):
 			names.append(0)
-		outbytes = unpack("iL", ioctl(sock.fileno(), 0x8912, pack("iL", _bytes, names.buffer_info()[0])))[0]  # 0x8912 = SIOCGIFCONF
+		outbytes = struct.unpack('iL', fcntl.ioctl(
+			s.fileno(),
+			0x8912,  # SIOCGIFCONF
+			struct.pack('iL', _bytes, names.buffer_info()[0])
+		))[0]
 		if outbytes == _bytes:
-			maxPossible *= 2
+			max_possible *= 2
 		else:
 			break
-	nameStr = names.tostring() if PY2 else names
+	namestr = names.tobytes()
 	ifaces = []
-	for index in range(0, outbytes, structSize):
-		ifaceName = bytes.decode(nameStr[index:index + 16]).split("\0", 1)[0].encode("ascii") if PY2 else str(nameStr[index:index + 16]).split("\0", 1)[0]
-		if ifaceName != "lo":
-			ifaces.append((ifaceName, inet_ntoa(nameStr[index + 20:index + 24])))
+	for i in range(0, outbytes, struct_size):
+		iface_name = bytes.decode(namestr[i:i + 16]).split('\0', 1)[0]
+		if iface_name != 'lo':
+			iface_addr = socket.inet_ntoa(namestr[i + 20:i + 24])
+			ifaces.append((iface_name, iface_addr))
 	return ifaces
 
 
